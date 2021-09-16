77a21de68217e3db96265ea39de64819d608bb85
lehieu099 <duyhieu099a2@gmail.com>
2021-09-16 11:46:03 +0700

    lecture_models

--- Migrations/20210916043933_Lecture.Designer.cs
+++ Migrations/20210916043933_Lecture.Designer.cs
@@ -2,14 +2,16 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Dotnet_Demo.Migrations
 {
     [DbContext(typeof(Dotnet_DemoContext))]
-    partial class Dotnet_DemoContextModelSnapshot : ModelSnapshot
+    [Migration("20210916043933_Lecture")]
+    partial class Lecture
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20210916043933_Lecture.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dotnet_Demo.Migrations
+{
+    public partial class Lecture : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lecture",
+                columns: table => new
+                {
+                    ID = table.Column<string>(type: "TEXT", nullable: false),
+                    LectureName = table.Column<string>(type: "TEXT", nullable: true),
+                    LectureHour = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lecture", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Lecture");
+        }
+    }
+}
