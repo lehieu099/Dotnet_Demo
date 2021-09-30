2901cf8e0380a219171bff2147a69e5c47bd74da
lehieu099 <duyhieu099a2@gmail.com>
2021-09-30 07:30:35 +0700

    30/09/2021

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Dotnet_Demo.Models{
     public class Movie{
@@ -7,9 +8,12 @@ namespace Dotnet_Demo.Models{
         public int ID { get; set; }
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
         public string DemoName { get; set; }
     }
