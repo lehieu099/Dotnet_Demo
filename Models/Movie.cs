2cddff1793910de128260a754839d54399f82482
lehieu099 <duyhieu099a2@gmail.com>
2021-10-07 08:00:42 +0700

    validation

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -5,16 +5,23 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace Dotnet_Demo.Models{
     public class Movie{
         [Key]
+        
         public int ID { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(30)]        
         public string Genre { get; set; }
 
         [Column(TypeName = "decimal(18, 2)")]
+        [Range(0, 500000), DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
         public string DemoName { get; set; }
     }
 }
